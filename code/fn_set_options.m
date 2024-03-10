f206be1e8c86b24897012b02078ad2d4c7f83060
danielpeppe <danjmpeppe@gmail.com>
2024-03-10 22:57:09 +0000

    fixed and better warnings

--- code/fn_set_options.m
+++ code/fn_set_options.m
@@ -6,15 +6,15 @@ function [op, op_output] = fn_set_options(op, op_output)
 default_op.abs_bdry_thickness_perc = 0.2;
 default_op.model_width_multiplier = 1.5;
 %Resolution
-default_op.els_per_wavelength = 30; %10 is default (increases are non-linear)
+default_op.els_per_wavelength = 15; %10 is default (increases are non-linear)
 default_op.time_step_safety_factor = 3; %3 is default
 %Signal options
 default_op.centre_freq = 5e6;
-default_op.no_cycles = 4;
+default_op.no_cycles = 3;
 default_op.max_time = 3.5e-6;
-default_op.src_matl = 'solid';
+default_op.src_matl = 'solid'; %solid, water, solid_horizontal
 default_op.src_dir = 2;
-default_op.aperture_n_els = 8; %number of elements
+default_op.aperture_n_els = 16; %number of elements
 default_op.separate_transmitter = 0; %by 1 element
 default_op.separate_receiver = 0;
 %Specimen
@@ -43,18 +43,18 @@ default_op.ply0_E_t_multiplier = 1;
 default_op.ply90_G_x_multiplier = 1;
 default_op.ply0_G_x_multiplier = 1;
 %   damping
-default_op.rayleigh_quality_factor = inf; %inf disables damping
+default_op.rayleigh_quality_factor = 0.5; %inf disables damping
 %Interply boundary options
 %   v1 and v2
 default_op.interply_boundary = 1;
 default_op.interply_layer1 = 'resin';
 default_op.interply_layer2 = 'resin';
-default_op.interply_el_thickness = 1;
+default_op.interply_el_thickness_perc = 0.05;
 %   v1
 default_op.interply_midway_boundary = 1;
 default_op.interply_every_layer = 1;
 %   v2
-default_op.interply_first_layer = 0;
+default_op.interply_first_layer = 1;
 default_op.interply_last_layer = 0;
 default_op.interply_rho_multiplier = 1;
 default_op.interply_D_multiplier = 1;
@@ -84,6 +84,9 @@ default_op.water_interface_perc = 0; %0-1 (1 separates transducer from specimen 
 default_op.water_interface_single = 0; %0 or 1 (1 separates transducer from specimen by 1 element)
 %Parameters for multiple simulations
 default_op.params = [];
+%Output scaling
+default_op.plot_scale_dsps = 1;
+default_op.plot_scale_time = 1;
 
 %% SET DEFAULT OPTIONS
 
@@ -91,38 +94,49 @@ op = fn_set_default_fields(op, default_op, true); %true to print non-default opt
 
 %% VALIDATE INPUT
 
+%turn off warning backtrace
+warning('OFF', 'BACKTRACE');
+
 %Resolution options
-if op.els_per_wavelength < 30
-    warning('els_per_wavelength < 30')
-elseif op.time_step_safety_factor < 3
+if op.els_per_wavelength < 15
+    warning('els_per_wavelength < 15')
+end
+if op.time_step_safety_factor < 3
     warning('time_step_safety_factor < 3')
 end
 %Input options
 if op.solid_specimen && op.composite_specimen
     error('Option Error: choose solid_specimen or composite_specimen')
-elseif (op.lower_water_present || op.upper_water_present) && (op.abs_bdry_thickness_perc >= op.water_bdry_thickness_perc)
+end
+if (op.lower_water_present || op.upper_water_present) && (op.abs_bdry_thickness_perc >= op.water_bdry_thickness_perc)
     warning('absorbing boundary >= water boundary')
 end
 %Ply options
 if op.n_plys_per_type == 0
     error('Option error: n_plys_per_type must be an integer > 0')
 end
-if ~op.interply_boundary && op.interply_el_thickness
+if ~op.interply_boundary && op.interply_el_thickness_perc
     error('Option error: set op.interply_boundary = 0 if op.interply_el_thickness != 0')
 end
 %Water options
 if op.water_interface_perc && op.water_interface_single
     error('Option Error: choose either op.water_interface_perc OR op.water_interface_single (not both)')
-elseif (op.water_interface_perc == 1 || op.water_interface_single == 1) && op.upper_water_present == 0
-    error('Option Error: op.upper_water_present == 0, set op.water_interface_perc/single == 0')
-elseif (op.lower_water_present || op.upper_water_present) && ~op.water_bdry_thickness_perc
+end
+if (op.water_interface_perc == 1 || op.water_interface_single == 1) && op.upper_water_present == 0
+    error('Option Error: op.upper_water_present = 0, set op.water_interface_perc/single = 0')
+end
+if (op.lower_water_present || op.upper_water_present) && ~op.water_bdry_thickness_perc
     error('Option Error: set op.water_bdry_thickness_perc > 0')
-elseif (~op.lower_water_present && ~op.upper_water_present) && op.water_bdry_thickness_perc
+end
+if (~op.lower_water_present && ~op.upper_water_present) && op.water_bdry_thickness_perc
     error('Option Error: set op.water_bdry_thickness_perc = 0')
 end
 %Solid water options
 if op.solidwater && ~op.upper_water_present
-    error('Option Error: set op.upper_water_present == 1 when using solidwater')
+    error('Option Error: set op.upper_water_present = 1 when using solidwater')
+end
+if (op.water_interface_perc || op.water_interface_single) && ~(strcmpi(op.src_matl,'water')) && ~op.solidwater
+    error('Option Error: set op.src_matl = ''water'' when using op.water_interface_perc or op.water_interface_single')
 end
 %Source options
 if strcmpi(op.src_matl,'water')
